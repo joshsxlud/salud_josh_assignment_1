ce21fca3aa89903613ef5bed674ca206bf0b8fc1
joshsxlud <jsalud2@academic.rrc.ca>
2024-08-30 11:52:03 -0500

    added the forgotten .gitignore file, updated the .py file to include a basic description.

--- Hello.py
+++ Hello.py
@@ -1,2 +1,10 @@
+"""
+Description: prompts user for name, and outputs a welcome message along with the inputed name
+Author: Josh Salud 
+Date: August 30, 2024
+
+"""
+
 name = input('what is your name\n')
+
 print ('hello,'" " + name)   
